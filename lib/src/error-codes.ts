3ff94ba075b83e0eb434ade32baec9f5cd542525
BABAH GEE <61418505+babahgee@users.noreply.github.com>
2025-10-15 15:20:11 +0200

    Added detailed debugging tools.

---
+++ lib/src/error-codes.ts
@@ -0,0 +1,11 @@
+export enum ErrorCodes {
+    NO_CONTEXT_PERMISSION = "FLUEXGL@AUDIO_ERROR_0001"
+}
+
+export enum WarningCodes {
+    NO_DEFAULT_AUDIO_DEVICE_FOUND = "FLUEXGL@AUDIO_WARNING_0001"
+}
+
+export enum SuccessCodes {
+
+}
---
+++ lib/src/utilities/debugger.ts
@@ -0,0 +1,53 @@
+import { ErrorCodes, WarningCodes } from "../error-codes";
+
+import { FluexGLAudio } from "../index";
+
+export namespace Debug {
+
+    export function Log(message: string, additionalDetails?: string[]) {
+
+        if(!FluexGLAudio.options.debugger.showInfo) return;
+
+        let outputString: string = `%c[INFO]: %c${message} %c`;
+
+        additionalDetails && additionalDetails.forEach(function (detail: string) {
+            outputString += `\n\u0009  > ${detail}`
+        });
+
+        return console.log(outputString, "color: white;", "color: gray", "color: gray");
+    }
+
+    export function Warn(message: string, additionalDetails?: string[], warningCode?: WarningCodes) {
+
+        if(!FluexGLAudio.options.debugger.showWarnings) return;
+
+        let outputString: string = `%c(${warningCode ? warningCode : "Unknown".toUpperCase()}) %c[WARNING]: %c${message} %c`;
+
+        (additionalDetails && additionalDetails !== null) && additionalDetails.forEach(function (detail: string) {
+            outputString += `\n\u0009  > ${detail}`
+        });
+
+        return console.log(outputString, "color: gray", "color: yellow;", "color: white", "color: white; font-style: italic;");
+    }
+
+    export function Error(message: string, additionalDetails?: string[], errorCode?: ErrorCodes) {
+
+        if(!FluexGLAudio.options.debugger.showErrors) return;
+        
+        let outputString: string = `%c(${errorCode ? errorCode : "Unknown".toUpperCase()}) %c[ERROR]: %c${message} %c`;
+
+        (additionalDetails && additionalDetails !== null) && additionalDetails.forEach(function (detail: string) {
+            outputString += `\n\u0009  > ${detail}`
+        });
+
+        console.log(outputString, "color: red", "color: red;", "color: white", "color: white; font-style: italic;");
+
+        if(FluexGLAudio.options.debugger.breakOnError) {
+
+            for(let char of message) 
+                outputString = outputString.replace("%c", "");
+
+            throw new window.Error(outputString);
+        }
+    }
+}
